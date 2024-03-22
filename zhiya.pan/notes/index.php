eb80b76429483a68149d18eb2cc971c7e6f66b2b
ziapan <p543665166@gmail.com>
2024-03-22 14:09:44 -0700

    notes

---
+++ zhiya.pan/notes/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	echo "<div>Hello World</div>";
+
+	$a = 5;
+
+	echo "I have $a apple";
+	echo 'I have $a';
+
+	//Number
+	$b = 15;
+
+
+	?>
+	
+</body>
+</html>
