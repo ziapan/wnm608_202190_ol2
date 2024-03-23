ee30f0b911f05087e2657d505be257ddfa38e6f9
Zhiya Pan <zhiyapan@ZhiyadeMacBook-Pro.local>
2024-03-22 23:23:07 -0700

    new world push

---
+++ zhiya.pan/notes/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php 
+	echo "<div>Hello World NEW</div>";
+
+	$a = 5;
+
+	echo "I have $a apple";
+	echo 'I have $a';
+
+	//Number
+	$b = 15;
+
+
+	?>
+	
+</body>
+</html>
