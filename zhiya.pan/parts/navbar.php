2c32eee5505cc0afc5a9c8a652990a55fb012265
ziapan <zhiyapan@ZhiyadeMacBook-Pro.local>
2024-03-23 22:22:57 -0700

    product-list

---
+++ zhiya.pan/parts/navbar.php
@@ -0,0 +1,34 @@
+<!-- <header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Shop</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</nav>
+
+
+
+		</div>
+	</header> -->
+
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<img class="navlogo" src="img/Logo.png" alt="Logo">
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="#index.html">Home</a></li>
+					<li><a href="#product_list.php">Shop</a></li>
+					<li><a href="#a">About</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
